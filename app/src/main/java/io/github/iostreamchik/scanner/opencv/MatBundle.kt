b404b78da24affdb7625a82d2328903d6b6a82cf
Eugene <iostream.includov@gmail.com>
2026-06-01 16:04:36 +0300

    fixed MatBundle

--- app/src/main/java/io/github/iostreamchik/scanner/opencv/MatBundle.kt
+++ app/src/main/java/io/github/iostreamchik/scanner/opencv/MatBundle.kt
@@ -3,31 +3,23 @@ package io.github.iostreamchik.scanner.opencv
 import org.opencv.core.Mat
 
 class MatBundle {
-    var gray: Mat? = null
-        get() = field ?: Mat()
-    var blurred: Mat? = null
-        get() = field ?: Mat()
-    var enhanced: Mat? = null
-        get() = field ?: Mat()
-    var morph: Mat? = null
-        get() = field ?: Mat()
-    var temp: Mat? = null
-        get() = field ?: Mat()
-    var edges: Mat? = null
-        get() = field ?: Mat()
-    var morphAdd: Mat? = null
-        get() = field ?: Mat()
-    var hierarchy: Mat? = null
-        get() = field ?: Mat()
+    var gray: Mat = Mat()
+    var blurred: Mat = Mat()
+    var enhanced: Mat = Mat()
+    var morph: Mat = Mat()
+    var temp: Mat = Mat()
+    var edges: Mat = Mat()
+    var morphAdd: Mat = Mat()
+    var hierarchy: Mat = Mat()
 
     fun releaseAll() {
-        gray?.release()
-        blurred?.release()
-        enhanced?.release()
-        morph?.release()
-        temp?.release()
-        edges?.release()
-        morphAdd?.release()
-        hierarchy?.release()
+        gray.release()
+        blurred.release()
+        enhanced.release()
+        morph.release()
+        temp.release()
+        edges.release()
+        morphAdd.release()
+        hierarchy.release()
     }
 }
